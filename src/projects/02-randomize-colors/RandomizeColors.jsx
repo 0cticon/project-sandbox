35abf0359ca892d8ec4f3a3728f446449fd93fa5
Kyle VanTassell <octicon@gmail.com>
2023-05-08 18:55:45 -0500

    sec button

--- src/projects/02-randomize-colors/RandomizeColors.jsx
+++ src/projects/02-randomize-colors/RandomizeColors.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import Title from "../components/Title";
 export default function RandomizeColors() {
+
+    function handleClick(event) {
+        // console.log("click");
+        console.log(event.target);
+    }
+
+    const handleSecClick = (e) => {
+        console.log(e.target);
+
+    };
     return (
-        <div className="container">
+        <div className="container m-auto text-center">
             <Title text={"Randomize Colors"} classes={"mb-4"} />
-            <button className='btn btn-danger'>Click me</button>
-            <button className='btn btn-success'>Click me</button>
+            <button
+                className='btn btn-danger'
+                onClick={(event) => handleClick(event)}
+            >
+                Click me
+            </button>
+            <button className='btn btn-success' onClick={handleSecClick}>Click me</button>
             <button className='btn btn-primary'>Click me</button>
             <button className='btn btn-warning'>Click me</button>
         </div>
