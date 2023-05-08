d43a4f4e7cf251305a0975da9f3f5b93684e3d39
Kyle VanTassell <octicon@gmail.com>
2023-05-08 18:32:07 -0500

    added buttons

--- src/projects/02-randomize-colors/RandomizeColors.jsx
+++ src/projects/02-randomize-colors/RandomizeColors.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Title from "../components/Title";
 export default function RandomizeColors() {
-    return (<div className="container">
-
-        <Title />
-        RandomizeColors
-    </div>
+    return (
+        <div className="container">
+            <Title text={"Randomize Colors"} classes={"mb-4"} />
+            <button className='btn btn-danger'>Click me</button>
+            <button className='btn btn-success'>Click me</button>
+            <button className='btn btn-primary'>Click me</button>
+            <button className='btn btn-warning'>Click me</button>
+        </div>
     );
 }
