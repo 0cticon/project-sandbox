a4fad1ae53638aa31f90b3be271ad2c1b2cde21c
Kyle VanTassell <octicon@gmail.com>
2023-05-08 18:15:46 -0500

    start of random color app

---
+++ src/projects/02-randomize-colors/RandomizeColors.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Title from "../components/Title";
+export default function RandomizeColors() {
+    return (<div className="container">
+
+        <Title />
+        RandomizeColors
+    </div>
+    );
+}
