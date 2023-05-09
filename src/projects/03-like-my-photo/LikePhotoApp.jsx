27faef528adef0ccd7b287767f436fd79cc88721
Kyle VanTassell <octicon@gmail.com>
2023-05-09 18:19:37 -0500

    added icons to footer of card, added imported image

--- src/projects/03-like-my-photo/LikePhotoApp.jsx
+++ src/projects/03-like-my-photo/LikePhotoApp.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import Title from "../components/Title";
-import { AiFillSmile } from "react-icons/ai";
+import { AiFillSmile, AiFillHeart, AiOutlineHeart, AiOutlineComment, } from "react-icons/ai";
+import DoggyDog from "./img/dog.jpeg";
 export default function LikePhotoApp() {
+    let like = false;
     return (
         <div className="container text-center">
             <Title text={"Like Photo App"} />
@@ -9,12 +11,17 @@ export default function LikePhotoApp() {
             <div className="card card-dark m-auto" style={{ width: 300, cursor: "pointer" }}
             >
                 <div className="card-header fs-xl">
-                    <AiFillSmile />
+                    <AiFillSmile className='mr-2' />
                     <small>DoggyDog</small>
                 </div>
-                <img src="" alt="img" style={{ height: "fit-content" }} />
+                <img src={DoggyDog} alt="img" style={{ height: "fit-content" }} />
 
-                <div className="card-footer fs-xl d-flex" style={{ justifyContent: "space-between" }}></div>
+                <div
+                    className="card-footer fs-xl d-flex"
+                    style={{ justifyContent: "space-between" }}
+                >
+                    <AiOutlineComment /> {like ? (<AiFillHeart className='text-danger' />) : (<AiOutlineHeart />)}
+                </div>
             </div>
 
         </div>
