355577b9b6beddaa59e3585401cecd0ff5068935
Kyle VanTassell <octicon@gmail.com>
2023-05-09 17:39:20 -0500

    add card and fit to avail space

--- src/projects/03-like-my-photo/LikePhotoApp.jsx
+++ src/projects/03-like-my-photo/LikePhotoApp.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import Title from "../components/Title";
 export default function LikePhotoApp() {
     return (
-        <div className="container">
-            <Title />
-            LikePhotoApp
+        <div className="container text-center">
+            <Title text={"Like Photo App"} />
+            <Title classes={"subtitle"} text={"Likes"} />
+            <div className="card card-dark m-auto" style={{ width: 300, cursor: "pointer" }}
+            >
+                <div className="card-header fs-xl">
+                    <small>DoggyDog</small>
+                </div>
+                <img src="" alt="img" style={{ height: "fit-content" }} />
+
+                <div className="card-footer fs-xl d-flex" style={{ justifyContent: "space-between" }}></div>
+            </div>
 
         </div>
     );
