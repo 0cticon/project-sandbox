d592311ec3dfc79388432ad1576af00705048532
Kyle VanTassell <octicon@gmail.com>
2023-05-16 18:21:15 -0500

    start testimonial app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
-import LikePhotoApp from './projects/03-like-my-photo/LikePhotoApp';
+import TestimonialsApp from './projects/04-testimonials-app/TestimonialsApp';
+// import LikePhotoApp from './projects/03-like-my-photo/LikePhotoApp';
 // import RandomizeColors from './projects/02-randomize-colors/RandomizeColors';
 // import EsignatureApp from './projects/01-e-signature-app/EsignatureApp';
 function App() {
   return (
     <div className="App">
       {/* <EsignatureApp /> */}
-
       {/* <RandomizeColors /> */}
-      <LikePhotoApp />
+      {/* <LikePhotoApp /> */} 
+      <TestimonialsApp />
     </div>
   );
 }
---
+++ src/projects/04-testimonials-app/TestimonialsApp.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export default function TestimonialsApp() {
+    return <div>TestimonialsApp</div>;
+
+}
