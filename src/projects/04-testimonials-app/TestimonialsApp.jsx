4b7d5d0bc9bbc65c5105b3bce40996ef42d6578a
Kyle VanTassell <octicon@gmail.com>
2023-05-17 19:27:41 -0500

    onClick now displays the text of button in subtitle

--- src/projects/04-testimonials-app/TestimonialsApp.jsx
+++ src/projects/04-testimonials-app/TestimonialsApp.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Title from "../components/Title";
 import Button from "../components/Button";
 import { BsFillFileEarmarkPostFill } from "react-icons/bs";
@@ -6,9 +6,10 @@ import { FaUserAlt } from "react-icons/fa";
 import { BiCommentDetail } from "react-icons/bi";
 
 export default function TestimonialsApp() {
-    const handleClick = () => {
-        console.log("My Click");
-    }
+    // const handleClick = () => {
+    //     console.log("My Click");
+    // };
+    const [testimonials, setTestimonials] = useState();
     return (
         <div className='container m-auto'>
             <Title text={"Testimonials App"} />
@@ -16,20 +17,21 @@ export default function TestimonialsApp() {
                 text={"Posts"}
                 btnClass="btn-info"
                 icon={<BsFillFileEarmarkPostFill />}
-                onClick={handleClick}
+                onClick={() => setTestimonials("Posts")}
             />{""}
             <Button
                 text={"Users"}
                 btnClass='btn-info'
                 icon={<FaUserAlt />}
-                onClick={handleClick}
+                onClick={() => setTestimonials("Users")}
             />
             <Button
-                text={"Comment"}
+                text={"Comments"}
                 btnClass='btn-info'
                 icon={<BiCommentDetail />}
-                onClick={handleClick}
+                onClick={() => setTestimonials("Comments")}
             />
+            <Title classes={"subtitle text-primary"} text={!testimonials ? "Select from above!" : testimonials} />
 
         </div>
     );
